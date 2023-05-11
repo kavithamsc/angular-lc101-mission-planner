64419de722169b12786e52c9452ff8f942e1b49e
Kavitha <kavithamsca@gmail.com>
2023-05-10 22:38:51 -0500

    changed experiments component

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[] = [
+    'Mars soil sample',
+    'Plant growth in habitat',
+   'Human bone density'
+    
+  ];
+  experimentsBeingEdited: string = null;
   constructor() { }
 
   ngOnInit() {
   }
-
+  add(list: string) {
+    this.experiments.push(list);
+  }
+  remove(list: string) {
+    let index = this.experiments.indexOf(list);
+    this.experiments.splice(index, 1);
+}
+edit(list: string) {
+  this.experimentsBeingEdited = list;
+}
+save(NewList: string, list: string) {
+  let index = this.experiments.indexOf(list)
+  this.experiments[index]=NewList;
+  }
 }
