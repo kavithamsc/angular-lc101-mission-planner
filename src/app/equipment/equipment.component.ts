1b7869a2a45603a5db9a68111861d6979323fce3
Kavitha <kavithamsca@gmail.com>
2023-05-10 22:27:28 -0500

    changed Equipment component

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-
+  equipment: string[] = [
+    'Habitat dome',
+    'Drones',
+   'Food containers',
+    'Oxygen tanks'
+  ];
+  equipmentBeingEdited: string = null;
   constructor() { }
 
   ngOnInit() {
   }
-
+  add(item: string) {
+    this.equipment.push(item);
+  }
+  remove(item: string) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+}
+edit(item: string) {
+  this.equipmentBeingEdited = item;
+}
+save(NewItem: string, item: string) {
+  let index = this.equipment.indexOf(item)
+  this.equipment[index]=NewItem;
+  }
 }
